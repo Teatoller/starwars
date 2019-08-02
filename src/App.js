edc51d5837934ba450e3dda9b8bf9b7a15aad28f
Teatoller <steven.otieno1@gmail.com>
2019-08-02 17:08:01 +0300

    (store props): implement store props in app
    - add the store props in the app component
    
    [Starts #004]

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <Provider store={store} >
+      <div className="App">
       <h1>Happy land</h1>
     </div>
+    </Provider>
+    
   );
 }
 
